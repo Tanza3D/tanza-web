b89f383f7e5c497ebc734a6d527c9e717ab14fa2
Tanza3D <hubsterhub@gmail.com>
2023-03-25 17:06:04 +0000

    stop the caching

--- global/php/functions.php
+++ global/php/functions.php
@@ -5,10 +5,10 @@ function Js($path, $local = true) {
     if($local == true) {
         $path = "/" . $path;
     }
-    echo '<script src="'.$path.'"></script>';
+    echo '<script src="'.$path.'?v=2"></script>';
 }
 function Css($path) {
-    echo '<link rel="stylesheet" href="/'.$path.'">';
+    echo '<link rel="stylesheet" href="/'.$path.'?v=2">';
 }
 function Redirect($url) {
     // redirects by placing js on the page
