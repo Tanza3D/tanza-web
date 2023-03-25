49dc13ee7fc6881df695e8808b03bbaf6b5cd1be
Tanza3D <hubsterhub@gmail.com>
2023-03-25 22:03:45 +0000

    who needs to wait

--- admin/global/js/global.js
+++ admin/global/js/global.js
@@ -1,3 +1,6 @@
+document.getElementById("page_loading_overlay").classList.add("loadingoverlay-hidden")
+document.body.classList.add("loaded"); // sometimes the event doens't call
+
 function quoteattr(s, preserveCR) {
     preserveCR = preserveCR ? '&#13;' : '\n';
     return ('' + s) /* Forces the conversion to string. */
@@ -10,8 +13,7 @@ function quoteattr(s, preserveCR) {
         You may add other replacements here for HTML only 
         (but it's not necessary).
         Or for XML, only if the named entities are defined in its DTD.
-        */ 
+        */
         .replace(/\r\n/g, preserveCR) /* Must be before the next replacement. */
-        .replace(/[\r\n]/g, preserveCR);
-        ;
+        .replace(/[\r\n]/g, preserveCR);;
 }
