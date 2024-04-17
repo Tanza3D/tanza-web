4eb6bee3f71047b39caefdc4c94570b24021bdf5
Tanza3D <hubsterhub@gmail.com>
2024-04-17 18:13:33 +0000

    quick update

--- api/endpoints/portfolio/items.php
+++ api/endpoints/portfolio/items.php
@@ -1,2 +1,6 @@
 <?php
-echo json_encode(Database::execSimpleSelect("SELECT * FROM Portfolio ORDER BY Date DESC"));
+if(isset($_GET['filter'])) {
+    echo json_encode(Database::execSelect("SELECT * FROM Portfolio WHERE Tag = ? ORDER BY Date DESC", "i", [$_GET['filter']]));
+} else {
+    echo json_encode(Database::execSimpleSelect("SELECT * FROM Portfolio ORDER BY Date DESC"));
+}   
